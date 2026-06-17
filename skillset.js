5c0ffc92b108e7e245573b99bb5fd094a4965fdf
nickleusjimenez <nickleus.jimenez@gmail.com>
2026-06-17 21:52:44 +0800

    Skillset change

--- skillset.js
+++ skillset.js
@@ -1,8 +1,8 @@
 var skillset = [
         "C#", "HTML", "CSS", "Javascript", "Typescript", "Sass",
         "Asp.net", "MS SQL", "Mysql", "Angular", "Git", "Azure", "Azure devops",
-        "JSON", "Docker", "IIS", "TFS", "Sitecore", "Entity Framework",
-        "WPF", "Dotnet MAUI", "Java", "Playwright", "Latex", "Confluence",
+        "JSON", "Docker", "IIS", "TFS", "Sitecore", "Entity Framework","YML",
+        "WPF", "Dotnet MAUI", "Java", "Playwright", "Latex", "Confluence","Postman","Newman",
         "Selenium", "Ollama", "Visual Basic .Net", "C", "JSP", "Markdown","Jira"
         ,"XML", "XAML", "Hyper-V","Powershell", "Bash", "Linux", "Windows Server"
       ];
